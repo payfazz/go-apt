90c727e212876c93c8fd7568e6034076d4551167
hantze <hantze.sudarma@payfazz.com>
2019-03-27 12:19:00 +0700

    resubmit interface for KV

--- pkg/fazzkv/interface.go
+++ pkg/fazzkv/interface.go
@@ -1,1 +1,10 @@
 package fazzkv
+
+// Store is an abstraction of key value storing data
+// it can be used for storing data to redis, memcached, etc.
+type Store interface {
+	Set(key string, value interface{}) error
+	Get(key string) (string, error)
+	Delete(key string) error
+	Truncate() error
+}
