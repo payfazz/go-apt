2134d64071d00e3b1be221c43d48314502af9142
vekaputra <valentino.ekaputra@payfazz.com>
2019-04-23 11:59:12 +0700

    feature: add response.Success

--- pkg/fazzcommon/response/response.go
+++ pkg/fazzcommon/response/response.go
@@ -8,9 +8,14 @@ import (
 	"net/http"
 )
 
-// Basic is a struct to contain default response message
-type Basic struct {
+// basicResponse is a struct to contain default response message
+type basicResponse struct {
 	Message string `json:"message"`
+}
+
+// successResponse is a struct to contain default success response
+type successResponse struct {
+	Success bool `json:"success"`
 }
 
 // Json is a function to return json object with given data and statusCode
@@ -23,7 +28,12 @@ func Json(w http.ResponseWriter, data interface{}, statusCode int) {
 
 // Text is a function to return raw text and statusCode
 func Text(w http.ResponseWriter, msg string, statusCode int) {
-	Json(w, Basic{Message: msg}, statusCode)
+	Json(w, basicResponse{Message: msg}, statusCode)
+}
+
+// Success is a function to return success status and statusCode
+func Success(w http.ResponseWriter, success bool, statusCode int) {
+	Json(w, successResponse{Success: success}, statusCode)
 }
 
 // Error is a function to return http error
