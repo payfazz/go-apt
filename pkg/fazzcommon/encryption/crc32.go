658915c7b86f710ca350da00c6d367ad0b56c1b8
hantze <hantze.sudarma@payfazz.com>
2019-04-26 16:14:32 +0700

    to lower crc32

--- pkg/fazzcommon/encryption/crc32.go
+++ pkg/fazzcommon/encryption/crc32.go
@@ -3,10 +3,11 @@ package encryption
 import (
 	"fmt"
 	"hash/crc32"
+	"strings"
 )
 
 // HashCRC32 is a function that used for hashing the message into CRC32
 func HashCRC32(message string) string {
 	crc32q := crc32.MakeTable(0xD5828281)
-	return fmt.Sprintf("%08x", crc32.Checksum([]byte(message), crc32q))
+	return strings.ToLower(fmt.Sprintf("%08x", crc32.Checksum([]byte(message), crc32q)))
 }
