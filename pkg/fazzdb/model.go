7150cf3c6eb564420e1ed2c7bb2081587651ab73
vekaputra <valentino.ekaputra@payfazz.com>
2019-05-06 15:51:47 +0700

    fix: use db tag when generate map payload

--- pkg/fazzdb/model.go
+++ pkg/fazzdb/model.go
@@ -245,7 +245,7 @@ func (m *Model) MapPayload(v ModelInterface) map[string]interface{} {
 				results[DELETED_AT] = model.DeletedAt
 			}
 		} else {
-			results[formatter.ToLowerFirst(classType.Field(i).Name)] = classValue.Field(i).Interface()
+			results[formatter.ToLowerFirst(classType.Field(i).Tag.Get("db"))] = classValue.Field(i).Interface()
 		}
 	}
 	return results
