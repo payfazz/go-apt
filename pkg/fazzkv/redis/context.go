b74d28378ac9440e8cb2425f442e9b65b43ac399
vekaputra <valentino.ekaputra@payfazz.com>
2019-05-14 20:20:33 +0700

    fix: cyclic dependency

--- pkg/fazzkv/redis/context.go
+++ pkg/fazzkv/redis/context.go
@@ -1,10 +1,8 @@
-package fazzkv
+package redis
 
 import (
 	"context"
 	"errors"
-
-	"github.com/payfazz/go-apt/pkg/fazzkv/redis"
 )
 
 type rdKeyType struct{}
@@ -13,14 +11,14 @@ var rdKey rdKeyType
 
 // NewRedisContext is a function to append redis object into context
 func NewRedisContext(ctx context.Context, addr string, password string) context.Context {
-	rds, _ := redis.NewFazzRedis(addr, password)
+	rds, _ := NewFazzRedis(addr, password)
 	return context.WithValue(ctx, rdKey, rds)
 }
 
 // GetRedisContext is a function to get redis object from context
 // Must be used after NewRedisContext
-func GetRedisContext(ctx context.Context) (redis.RedisInterface, error) {
-	rds := ctx.Value(rdKey).(redis.RedisInterface)
+func GetRedisContext(ctx context.Context) (RedisInterface, error) {
+	rds := ctx.Value(rdKey).(RedisInterface)
 	if nil == rds {
 		return nil, errors.New("redis instance not found in context, make sure to call NewRedisContext before calling GetRedisContext")
 	}
