bcb21fdb1ea8c4cebc73f918f6428f1383700497
M. Febrian Ramadhana <febrian.ramadhana@payfazz.com>
2019-08-06 16:21:55 +0700

    fix: using non-uuid aggregate id in postgres event store

--- pkg/esfazz/eventstore/eventpostgres/migration.go
+++ pkg/esfazz/eventstore/eventpostgres/migration.go
@@ -7,7 +7,7 @@ func CreateEventsTable(name string) *fazzdb.MigrationTable {
 	return fazzdb.CreateTable(name, func(table *fazzdb.MigrationTable) {
 		table.Field(fazzdb.CreateBigSerial("event_id").Primary())
 		table.Field(fazzdb.CreateString("event_type"))
-		table.Field(fazzdb.CreateUuid("aggregate_id"))
+		table.Field(fazzdb.CreateString("aggregate_id"))
 		table.Field(fazzdb.CreateInteger("aggregate_version"))
 		table.Field(fazzdb.CreateJsonB("data"))
 		table.TimestampsTz(0)
