4038048ada74fa93c2900d9faa1c5eeb99744c66
Valentino Ekaputra <silentrazgriz@gmail.com>
2019-12-11 14:14:34 +0700

    test: fix test for fazzcloud

--- pkg/fazzcloud/http_test.go
+++ pkg/fazzcloud/http_test.go
@@ -15,7 +15,7 @@ func TestHTTPClient_Get(t *testing.T) {
 	params["format"] = "json"
 	headers["Content-Type"] = "multipart/form-data"
 
-	httpClient := NewHTTPClient("https://api.ipify.org")
+	httpClient := NewHTTPClient("https://api.ipify.org", nil)
 
 	_, _, err := httpClient.Get("", &params, &headers)
 	if err != nil {
@@ -24,14 +24,14 @@ func TestHTTPClient_Get(t *testing.T) {
 }
 
 func TestFailedHTTPClient_Get(t *testing.T) {
-	httpClient := NewHTTPClient("https:/api.ipify.asdf")
+	httpClient := NewHTTPClient("https:/api.ipify.asdf", nil)
 
 	_, _, err := httpClient.Get("", nil, nil)
 	if err == nil {
 		t.Fatalf("failed to test failed http client get")
 	}
 
-	httpClient = NewHTTPClient("http://dummy.restapiexample.com/api/v1/create")
+	httpClient = NewHTTPClient("http://dummy.restapiexample.com/api/v1/create", nil)
 	_, _, err = httpClient.Get("", nil, nil)
 	httpClient.TraceRequest()
 	if err == nil {
@@ -43,7 +43,7 @@ func TestHTTPClient_Send(t *testing.T) {
 	var headers = make(map[string]string)
 	headers["Content-Type"] = JSON
 
-	httpClient := NewHTTPClient("http://dummy.restapiexample.com/api/v1")
+	httpClient := NewHTTPClient("http://dummy.restapiexample.com/api/v1", nil)
 	s1 := rand.NewSource(time.Now().UnixNano())
 	r1 := fmt.Sprintf("%d", rand.New(s1))
 
@@ -68,7 +68,7 @@ func TestHTTPClient_Send(t *testing.T) {
 }
 
 func TestFailedHTTPClient_Send(t *testing.T) {
-	httpClient := NewHTTPClient("http://dummy.restapiexample.co.id/api/v1")
+	httpClient := NewHTTPClient("http://dummy.restapiexample.co.id/api/v1", nil)
 	tempData := struct {
 		Name   string `json:"name"`
 		Salary string `json:"salary"`
@@ -100,7 +100,7 @@ func TestHTTPClient_Delete(t *testing.T) {
 	params["format"] = "json"
 	headers["Content-Type"] = "application/json"
 
-	httpClient := NewHTTPClient("http://dummy.restapiexample.com/api/v1")
+	httpClient := NewHTTPClient("http://dummy.restapiexample.com/api/v1", nil)
 
 	_, _, err := httpClient.Delete("delete/2", &headers)
 	if err != nil {
@@ -115,7 +115,7 @@ func TestFailedHTTPClient_Delete(t *testing.T) {
 	params["format"] = "json"
 	headers["Content-Type"] = "application/json"
 
-	httpClient := NewHTTPClient("http://dummy.restapiexample.co.id/api/v1")
+	httpClient := NewHTTPClient("http://dummy.restapiexample.co.id/api/v1", nil)
 
 	_, _, err := httpClient.Delete("delete/2", &headers)
 	if err == nil {
