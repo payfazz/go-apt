070e5d9695aa7b185e59222007db9f09097c5721
Valentino Ekaputra <silentrazgriz@gmail.com>
2020-01-20 22:20:35 +0700

    enhancement: separate actual prometheus Increment and Observe into another method

--- pkg/fazzmonitor/prometheusclient/requestcounter.go
+++ pkg/fazzmonitor/prometheusclient/requestcounter.go
@@ -1,33 +1,69 @@
 package prometheusclient
 
 import (
+	"fmt"
 	"net/http"
+	"sync"
 
+	"github.com/payfazz/go-apt/pkg/fazzrouter"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
+var httpCounterOnce sync.Once
+var httpCounter *prometheus.CounterVec
+
 func httpRequestCounter() *prometheus.CounterVec {
-	counter := prometheus.NewCounterVec(
-		prometheus.CounterOpts{
-			Name: "http_requests_total",
-			Help: "A counter for requests to the wrapped handler.",
-		},
-		[]string{"service", "path", "method", "code"},
-	)
+	httpCounterOnce.Do(func() {
+		httpCounter = prometheus.NewCounterVec(
+			prometheus.CounterOpts{
+				Name: "http_requests_total",
+				Help: "A counter for requests to the wrapped handler.",
+			},
+			[]string{"service", "path", "method", "code"},
+		)
 
-	prometheus.MustRegister(counter)
+		prometheus.MustRegister(httpCounter)
+	})
 
-	return counter
+	return httpCounter
 }
 
-func RequestCounter(serviceName string, pattern RoutePattern) func(next http.HandlerFunc) http.HandlerFunc {
-	counter := httpRequestCounter()
-
+// HTTPRequestCounterMiddleware middleware wrapper for IncrementRequestCounter, recommended to be used if you are using `go-apt/pkg/fazzrouter` package, the only thing required: before using this middleware make sure you use `kv.New()` middleware from `github.com/payfazz/go-middleware`
+// required params:
+// - productName: your product / team name (snake_case)
+// - serviceName: your service name (snake_case)
+func HTTPRequestCounterMiddleware(productName string, serviceName string) func(next http.HandlerFunc) http.HandlerFunc {
 	return func(next http.HandlerFunc) http.HandlerFunc {
 		return func(writer http.ResponseWriter, req *http.Request) {
 			prometheusWriter := wrapResponseWriter(writer)
+
 			next(prometheusWriter, req)
-			counter.With(labels(serviceName, prometheusWriter, req, pattern)).Inc()
+
+			IncrementRequestCounter(
+				productName,
+				serviceName,
+				fazzrouter.GetPattern(req),
+				req.Method,
+				prometheusWriter.Code(),
+			)
 		}
 	}
+}
+
+// IncrementRequestCounter increment http request count and store it as total requests, usage example can be seen in HTTPRequestCounterMiddleware method
+// required params:
+// - productName: your product / team name (snake_case)
+// - serviceName: your service name (snake_case)
+// - pattern: your route pattern not the requested url, ex: `/v1/users/:id` (correct); `/v1/users/{id}` (correct); `/v1/users/1` (incorrect)
+// - method: your http request method (GET, POST, PATCH, etc)
+// - code: your http status code (200, 404, 500, etc)
+func IncrementRequestCounter(productName string, serviceName string, pattern string, method string, code string) {
+	service := fmt.Sprintf("%s_%s", productName, serviceName)
+	labels := prometheus.Labels{
+		"service": service,
+		"path":    pattern,
+		"method":  method,
+		"code":    code,
+	}
+	httpRequestCounter().With(labels).Inc()
 }
