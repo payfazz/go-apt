4217801cd3598698e44aba7959815227736882e4
azharprabudi <azharprabui@gmail.com>
2021-02-17 14:48:23 +0700

    rename code to be status at prometheus http request label

--- pkg/fazzmonitor/prometheusclient/requestcounter.go
+++ pkg/fazzmonitor/prometheusclient/requestcounter.go
@@ -16,7 +16,7 @@ func httpRequestCounter() *prometheus.CounterVec {
 				Name: "http_requests_total",
 				Help: "A counter for requests to the wrapped handler.",
 			},
-			[]string{"path", "method", "code"},
+			[]string{"path", "method", "status"},
 		)
 
 		prometheus.MustRegister(httpCounter)
@@ -30,12 +30,12 @@ func httpRequestCounter() *prometheus.CounterVec {
 // - serviceName: your service name (snake_case)
 // - pattern: your route pattern not the requested url, ex: `/v1/users/:id` (correct); `/v1/users/{id}` (correct); `/v1/users/1` (incorrect)
 // - method: your http request method (GET, POST, PATCH, etc)
-// - code: your http status code (200, 404, 500, etc)
-func IncrementRequestCounter(pattern string, method string, code string) {
+// - status: your http status code (200, 404, 500, etc)
+func IncrementRequestCounter(pattern string, method string, status string) {
 	labels := prometheus.Labels{
 		"path":   pattern,
 		"method": method,
-		"code":   code,
+		"status": status,
 	}
 	httpRequestCounter().With(labels).Inc()
 }
